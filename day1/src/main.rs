ceed6891925bcae0cbac0b293b593e081e151bbb
Cody <cody.abbott16@gmail.com>
2023-12-03 20:31:29 +0000

    additional passing tests

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,13 +7,17 @@ const FILE_PATH_LOCATION: &str =
 
 pub fn generate_line_number(line: &str) -> u32 {
     let mut left_number: u32 = 0;
+    let mut left_number_populated: bool = false;
     let mut right_number: u32 = 0;
 
     for c in line.chars() {
         if c.is_numeric() {
             let digit = c.to_digit(10).unwrap();
-            left_number = digit;
-            break;
+            if left_number_populated == false {
+                left_number = digit;
+            }
+            right_number = digit;
+            left_number_populated = true;
         }
     }
 
@@ -23,7 +27,8 @@ pub fn generate_line_number(line: &str) -> u32 {
 fn main() {
     println!("Reading input from file location: {}", FILE_PATH_LOCATION);
 
-    let file_contents = fs::read_to_string(FILE_PATH_LOCATION).expect("Failed to open file!");
+    let file_contents: String =
+        fs::read_to_string(FILE_PATH_LOCATION).expect("Failed to open file!");
     println!("{}", file_contents);
 
     let split_lines = file_contents.lines();
--- day1/src/tests.rs
+++ day1/src/tests.rs
@@ -3,8 +3,14 @@ mod tests {
     use crate::generate_line_number;
 
     #[test]
-    fn returns_expected_total() {
-        let response = generate_line_number("1abc2");
-        assert_eq!(response, 10);
+    fn returns_two_digit_total() {
+        let response: u32 = generate_line_number("1abc2");
+        assert_eq!(response, 12);
+    }
+
+    #[test]
+    fn returns_one_digit_total() {
+        let response: u32 = generate_line_number("a7bc");
+        assert_eq!(response, 77);
     }
 }
